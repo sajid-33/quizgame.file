6c25817919d0b9e9da60da1074692995a4da3975
Sajid IN <insajidinsajid33@gmail.com>
2025-06-01 19:24:12 +0530

    Update myquizprgrm.py
    modified

--- myquizprgrm.py
+++ myquizprgrm.py
@@ -1,6 +1,10 @@
-# Ask the user for the size of the square
-size = int(input("Enter the size of the square: "))
+# Print uppercase letters A-Z
+print("Uppercase Letters:")
+for i in range(65, 91):  # ASCII values of A-Z
+    print(chr(i), end=' ')
+print("\n")
 
-# Print the square
-for i in range(size):
-    print("* " * size)
+# Print lowercase letters a-z
+print("Lowercase Letters:")
+for i in range(97, 123):  # ASCII values of a-z
+    print(chr(i), end=' ')
